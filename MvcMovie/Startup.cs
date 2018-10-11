15bcd5c995f9416cd46cc9d642e34f5155330ff4
FranciscoFrade <150221002@estudantes.ips.pt>
2018-10-11 19:19:06 +0100

    5. Work with SQL Server LocalDB

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -37,7 +37,7 @@ namespace MvcMovie
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<MvcMovieContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
